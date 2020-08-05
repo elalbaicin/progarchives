719f5a48d43acc5a21d9eaa9e58ce723476c1c4c
elalbaicin <67240950+elalbaicin@users.noreply.github.com>
2020-08-05 18:03:51 -0300

    Simplificação

--- scrape_progarchives.R
+++ scrape_progarchives.R
@@ -11,20 +11,6 @@ suppressPackageStartupMessages(library(rebus))
 # Opção de paralelismo do pacote furrr
 plan(multiprocess)
 
-# Carregue as funções coletoras
-source("scraping_functions.R")
-
-# Função auxiliar
-fill_empty_listcol <- as_mapper(~ if(is.null(.x) || nrow(.x) == 0){
-  
-  return(NA)
-  
-} else {
-  
-  return(.x)
-  
-})
-
 # Extração dos dados ------------------------------------------------------
 
 # Identifique os subgêneros e suas URLs
@@ -42,19 +28,17 @@ main <- main %>%
   mutate(album_data = future_map(.x = url_artist,
                                  .f = extract_albums,
                                  .progress = TRUE)) %>% 
-  mutate(album_data = map(.x = album_data,
-                          .f = fill_empty_listcol)) %>% 
-  unnest(cols = c(album_data)) %>% 
+  unnest(cols = c(album_data),
+         keep_empty = TRUE) %>% 
   remove_empty(which = "cols")
 
 # # Tome a distribuição da notas de cada lançamento - o processo é demorado
 # main <- main %>%
 #   mutate(ratings = future_map(.x = url_album,
 #                               .f = extract_ratings,
-#                               .progress = TRUE)) %>% 
-#   mutate(ratings = map(.x = ratings,
-#                        .f = fill_empty_listcol)) %>% 
-#   unnest(cols = c(ratings)) %>% 
+#                               .progress = TRUE)) %>%
+#   unnest(cols = c(ratings),
+#          keep_empty = TRUE) %>%
 #   remove_empty(which = "cols")
 
 # Salve os dados
